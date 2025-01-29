f665da4ed6aa516f97b62681eb7353657c067adf
chriswynns1 <98430976+chriswynns1@users.noreply.github.com>
2025-01-29 13:16:52 -0800

    Create installing-k3s.d

---
+++ installing-k3s.d
@@ -0,0 +1,13 @@
+# Steps I took to install k3s:
+
+## server setup:
+- install ubuntu server as normal
+- configure port 1 on second nic to use routes:
+```routes:
+     - to: 0.0.0.0/0
+       via: 192.168.1.1/24```
+- update, upgrade, and install k3s:
+```sudo apt update
+   sudo apt upgrade
+   curl -sfL https://get.k3s.io | sh -
+```
